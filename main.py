b6e1e2dcb22b6657ce24a147bae672f1a4883236
kyaurix <your.email@example.comnabeelul101@gmail.com>
2026-06-12 16:38:36 +0100

    Add team ownership table and match scoring function

---
+++ main.py
@@ -0,0 +1,58 @@
+team_to_owner = {
+    "Belgium": "nabeel",
+    "USA": "nabeel",
+    "Panama": "nabeel",
+    "Iraq": "nabeel",
+
+    "France": "abu",
+    "Iran": "abu",
+    "Czechia": "abu",
+    "Scotland": "abu",
+
+    "Portugal": "nasir",
+    "Mexico": "nasir",
+    "Canada": "nasir",
+    "Uzbekistan": "nasir",
+
+    "Croatia": "shah",
+    "Colombia": "shah",
+    "Paraguay": "shah",
+    "Cape Verde": "shah",
+
+    "Argentina": "nadim",
+    "Senegal": "nadim",
+    "Algeria": "nadim",
+    "Qatar": "nadim",
+
+    "Brazil": "yusuf",
+    "Switzerland": "yusuf",
+    "Norway": "yusuf",
+    "DR Congo": "yusuf",
+
+    "Netherlands": "ishraq",
+    "Japan": "ishraq",
+    "Sweden": "ishraq",
+    "South Africa": "ishraq",
+
+    "England": "hameem",
+    "Uruguay": "hameem",
+    "Egypt": "hameem",
+    "Jordan": "hameem",
+
+    "Germany": "musaddik",
+    "Ecuador": "musaddik",
+    "South Korea": "musaddik",
+    "Bosnia": "musaddik",
+
+    "Morocco": "mahir",
+    "Austria": "mahir",
+    "Australia": "mahir",
+    "Saudi Arabia": "mahir",
+
+    "Spain": "nahid",
+    "Türkiye": "nahid",
+    "Ivory Coast": "nahid",
+    "Tunisia": "nahid"
+}
+
+print(team_to_owner["Spain"])
